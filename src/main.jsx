ee8ed1f2d2549316a4527e2a5f6000af7c31eae0
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-09-05 19:33:32 +0200

    refactored rootlayout page

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./routes/RootLayout.jsx";
 import NotFoundPage from "./routes/NotFoundPage.jsx";
+import RootLayout from "./routes/RootLayout.jsx";
 import Home from "./routes/Home.jsx";
 import Cart from "./routes/Cart.jsx";
 import Shop from "./routes/Shop.jsx";
