6e878a802a149e4316372dc424510f871baa5ff3
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-09-05 19:24:22 +0200

    deleted qr code

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import { qrcode } from "vite-plugin-qrcode";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), qrcode()],
+  plugins: [react(), tailwindcss()],
 });
